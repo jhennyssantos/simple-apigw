808c73e6fd3949022372b76648ff098a0ffe0791
jhennyssantos <jhenifersantos.si@gmail.com>
2022-01-09 13:48:38 -0300

    Add url path parameters

--- api.tf
+++ api.tf
@@ -23,6 +23,7 @@ resource "aws_api_gateway_method" "item_method" {
   authorization = "NONE"
   request_parameters = {
     "method.request.header.Content-Type" = true
+    "method.request.path.item" = true
   }
 }
 
@@ -34,6 +35,10 @@ resource "aws_api_gateway_integration" "item_integration" {
   type                    = "AWS"
   credentials             = aws_iam_role.simple_apigw_role.arn
   uri                     = "arn:aws:apigateway:us-east-1:s3:path/${aws_s3_bucket.simple_apigw_s3.bucket}/{item}"
+  request_parameters = {
+    "integration.request.path.item" = "method.request.path.item"
+  }
+  
 }
 
 resource "aws_api_gateway_method_response" "item_response_200" {
@@ -66,7 +71,20 @@ resource "aws_api_gateway_method" "item_method_del" {
   http_method   = "DELETE"
   authorization = "NONE"
   request_parameters = {
-    "method.request.header.Content-Type" = true
-  }
+      "method.request.path.item" = true
+   }
+}
+
+resource "aws_api_gateway_integration" "item_integration_del" {
+  rest_api_id             = aws_api_gateway_rest_api.simple_apigw.id
+  resource_id             = aws_api_gateway_resource.bucket_item_resource.id
+  http_method             = aws_api_gateway_method.item_method_del.http_method
+  integration_http_method = "DELETE"
+  type                    = "AWS"
+  credentials             = aws_iam_role.simple_apigw_role.arn
+  uri                     = "arn:aws:apigateway:us-east-1:s3:path/${aws_s3_bucket.simple_apigw_s3.bucket}/{item}"
+  request_parameters = {
+      "integration.request.path.item" = "method.request.path.item"
+   }
 }
 
